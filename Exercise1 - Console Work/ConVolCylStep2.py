985bcc672722c928d061bbd1393032e84f32ae78
Andrew Ma <andrew.ma@andrewma.local>
2019-09-19 15:23:28 -0400

    Build 0.4

--- Exercise1 - Console Work/ConVolCylStep2.py
+++ Exercise1 - Console Work/ConVolCylStep2.py
@@ -1,5 +1,8 @@
 import math
 print("Volume of a Cylinder Formula: ")
+print("V = pi * radius *radius * height")
+print("You will need the radius and height")
+print("to calculate the volume.") 
 #Input
 #What inputs are needed to calculate the volume of a cylinder?
 name = input("What is your name: ")   #takes users name
@@ -13,6 +16,11 @@ height = float(height)     #cast to int
 #Process
 #What formula is used to calculate the volume of a cylinder?
 volume = math.pi*radius*radius*height
+volume = round(volume,2)
 #Output
 #What is important about the output?
-print ("Hi, "+name+"! The Answer is "+str(volume))
+print ("Hi, "+name+")
+print ("Cylinder Information:")
+print ("Radius = " +str(radius))
+print ("Height = " +str(height))
+print ("Volume = "+str(volume)+"\n")
